72c377c8e4d2188f2049cb2505c35f1684192d24
alexxcalle <axcalle@sudamericano.edu.ec>
2025-02-26 11:29:17 -0500

    finish landing and add traslation

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,8 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   outDir: './dist',
+  i18n: {
+    locales: ['pl', 'en'],
+    defaultLocale: 'pl',
+  },
 });
---
+++ src/i18n.ts
@@ -0,0 +1,8 @@
+// filepath: /src/i18n.ts
+import i18next from "i18next";
+
+i18next.init({
+  // … your configuration …
+});
+
+export default i18next;
